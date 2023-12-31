903b58e7be59a9009cd55299eec88b4a49268744
nvhoang2003 <nvhoang12082003@gmail.com>
2023-12-31 23:54:54 +0700

    code

---
+++ ShowroomManagement/Models/PurchaseResposneDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShowroomManagement.Models
+{
+    public class PurchaseResposneDTO
+    {
+        public int purchaseId { get; set; }
+        public int vehicleId { get; set; }
+        public string fullName { get; set; }
+        public DateTime datePurchase { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public string modelNumber { get; set; }
+        public string color { get; set; }
+    }
+}
