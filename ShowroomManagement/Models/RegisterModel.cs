bb604d52ca428a4e615dc69bbdf518a58220dc04
duongsang95 <duongsang.ntl95@gmail.com>
2024-01-09 08:46:26 +0700

    add

---
+++ ShowroomManagement/Models/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShowroomManagement.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Please fill in the information")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "The username must have at least 6 characters")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Please fill in the information")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "The password must contain at least 8 characters")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Please re-enter the password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string confirmPassword { get; set; }
+    }
+}
